05f27144a33e25185ca4269f56b5210021dcc54a
cflux1990 <cflux.maltez@live.com>
2018-10-16 07:43:36 -0700

    24.390 - Using CSS Animations

--- src/App.js
+++ src/App.js
@@ -6,24 +6,24 @@ import Backdrop from "./components/Backdrop/Backdrop";
 import List from "./components/List/List";
 
 class App extends Component {
-    state = {
-        modalIsOpen: false
-    }
-    showModal = () => {
-      this.setState({modalIsOpen: true})
-    }
+  state = {
+    modalIsOpen: false
+  }
 
-    closeModal = () => {
-        this.setState({modalIsOpen: false})
-    }
+  showModal = () => {
+    this.setState({modalIsOpen: true});
+  }
 
+  closeModal = () => {
+    this.setState({modalIsOpen: false});
+  }
 
   render() {
     return (
       <div className="App">
         <h1>React Animations</h1>
-        <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
-        <Backdrop />
+        <Modal show={this.state.modalIsOpen} closed={this.closeModal}/>
+        <Backdrop show={this.state.modalIsOpen} />
         <button className="Button" onClick={this.showModal}>Open Modal</button>
         <h3>Animating Lists</h3>
         <List />
